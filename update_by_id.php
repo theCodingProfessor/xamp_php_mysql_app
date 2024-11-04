b15b38d1a976625738b1bfff03eda2eb990a1f93
demo admin <no@at.such>
2024-11-04 16:08:03 -0600

    update database story with corrected namespace data

--- update_by_id.php
+++ update_by_id.php
@@ -6,20 +6,20 @@ $str_key = strval($get_key);
 $str_data = strval($get_data);
 
 $servername = "localhost";
-$username = "reader";
-$password = "reader";
-$dbname = "my_xamp";
+$username = "read_and_write";
+$password = "read_and_write";
+$my_db = "my_xampp_new";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $my_db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 else {
-    $stmt = $conn->prepare("UPDATE my_table SET save_text = (?) WHERE my_key = (?)");
+    $stmt = $conn->prepare("UPDATE xampp_table SET user_data = (?) WHERE id = (?)");
     $stmt->bind_param("ss", $str_data, $str_key);
     $stmt->execute();
-    echo "Connected successfully";
+    // echo "Connected successfully";
 }
 header("Location: ./read_db.php");
 
